9d4d455bea4471b1d7f78098c488cfc16a044597
Søren Lund <soren.lund@hotmail.com>
2018-04-07 14:47:21 +0200

    Adds ContactForm ViewModel, Contact Partial + minor tweaks to footer and map.

---
+++ Portfolio/ViewModels/ContactForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.ViewModels
+{
+    public class ContactForm
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Email")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Phone]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please write a message")]
+        public string Message { get; set; }
+    }
+}
